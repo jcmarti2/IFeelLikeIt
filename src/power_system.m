0a49ad45e3e769fda861e332007363ce3c157ec4
jcmarti2 <jcmarti2@illinois.edu>
2016-04-24 02:36:21 -0500

    Cleaned up entire code, started building power_system class. Constructor is finished, working on cost and emissions linear opt functions.

--- src/power_system.m
+++ src/power_system.m
@@ -1,160 +1,3 @@
-% Given parameters
-x_i_max = [240 210 180 400 500 500 320 400 200]';
-x_i_min = [70 0 0 100 0 0 0 0 0]';
-o_i_u = [0.05 0.04 0.035 0.05 0.05 0.05 0.035 0.03 0.10]';
-o_i_p = [0.15 0.15 0.50 0.10 0.15 0.13 0.70 0.8 0.15]';
-g_i = [1020 766 435 16 392 305 15 45 80]';
-
-c_i_v = [25.8 29.1 79.8 11.5 36.9 44.3 0.95 1.0 1.5]';
-sigma_i_v = [3.0 3.0 3.0 4.0 2.0 2.0 0.1 0.5 0.25]';
-c_i_c = [0 0 0 0 93.4 17.8 83.4 145.0 0]';
-sigma_i_c = [0 0 0 0 9 4 18 20 0]';
-
-n_t = [106 310 670 1250 2780 3650];
-l = [1390 1005 930 845 455 260];
-
-s_kt_max = [ 80  35 20 15   5 0;
-             70  30 15 10.5 5 0;
-             100 0  0  0    0 0];
-c_k_d = [55 70 100]';
-sigma_k_d = [15 4 20]';
-
-
-% Space to store A and b 
-A = zeros(157,61);
-b = NaN(157,1);
-
-% Matrix to store x_it indices for each row of A
-x_it = zeros(6,9); % CHANGE NAME LATER TO x_ti
-for i = 1:numel(x_it)
-    x_it(i) = i; 
-end
-
-% Vector to store y_i indices for each row of A
-y_i = NaN(1,9)';
-y_i(5:8) = [55 56 57 58]';
-
-% Vector to store z_k indices for each row of A
-z_k = [59 60 61];
-
-
-% Building matrix A and vector b
-idx = 1;
-
-% Load constraint (6 rows)
-% sum_i=1^9{x_it} + sum_k=1^3{s_kt_max} >= l_t for t={1,2,3,4,5,6}
-for t = 1:6
-    A(idx,x_it(t,:)) = -1;
-    A(idx,z_k) = -s_kt_max(:,t)';
-    b(idx) = -l(t);
-    idx = idx + 1;
-end
-
-% Instantaneous capacity constraint (54 rows)
-% x_it <= (1-o_i_u)x_i_max for i={1,2,3,4,5,6,7,8,9}, t={1,2,3,4,5,6}
-for i = 1:9
-    for t = 1:6
-        A(idx,x_it(t,i)) = 1;
-        b(idx) = (1-o_i_u(i))*x_i_max(i);
-        idx = idx + 1; 
-    end
-end 
-
-% Annual energy constraint for exisiting plants (5 rows)
-% sum_t=1^6{n_tx_it} - (1-o_i_p)8766x_i_max <= for i={1,2,3,4,9}
-for i = 1:4
-    A(idx,x_it(:,i)') = n_t;
-    b(idx) = (1-o_i_p(i))*x_i_max(i)*8766;
-    idx = idx + 1;
-end
-A(idx,x_it(:,9)') = n_t;
-b(idx) = (1-o_i_p(9))*x_i_max(9)*8766;
-idx = idx + 1;
-
-% Annual energy constraint for new plants (4 rows)
-% sum_t=1^6{n_tx_it} - (1-o_i_p)8766y_i <= for i={5,6,7,8}
-for i = 5:8
-    A(idx,x_it(:,i)') = n_t;
-    A(idx,y_i(i)) = -8766*(1-o_i_p(i));
-    b(idx) = 0;
-    idx = idx + 1;
-end
-
-% Minimum generation constraint (54 rows)
-% x_it >= x_i_min for i={1,2,3,4,5,6,7,8,9}, t={1,2,3,4,5,6}
-for i = 1:9
-    for t = 1:6
-        A(idx,x_it(t,i)) = -1;
-        b(idx) = -x_i_min(i);
-        idx = idx + 1;
-    end
-end 
-
-% Lower bound on DSM program (3 rows)
-% z_k >= 0 for k={1,2,3}
-for k = 1:3
-    A(idx,z_k(k)) = -1;
-    b(idx) = 0;
-    idx = idx + 1;
-end
-
-% Upper bound on DSM program (3 rows)
-% z_k <= 1 for k={1,2,3}
-for k = 1:3
-    A(idx,z_k(k)) = 1;
-    b(idx) = 1;
-    idx = idx + 1;
-end
-
-% New generation bounds (24 rows)
-% x_it <= (1-o_i_u)y_i for i={5,6,7,8}, t={1,2,3,4,5,6}
-for i = 5:8
-    for t = 1:6
-        A(idx,x_it(t,i)) = 1;
-        A(idx,y_i(i)) = -(1-o_i_u(i));
-        b(idx) = 0;
-        idx = idx + 1;
-    end
-end 
-
-% New generation bounds (4 rows)
-% y_i <= x_i_max for i={5,6,7,8}
-for i = 5:8
-    A(idx,y_i(i)) = 1;
-    b(idx) = x_i_max(i);
-    idx = idx + 1;
-end
-
-
-
-
-c_prime = [c_i_v' c_i_c(5:8)' c_k_d']';
-
-H = zeros(16,61);
-for i = 1:9
-    H(i,1+6*(i-1):6+6*(i-1)) = n_t;
-end
-H(10:13,55:58) = 1000*eye(4,4);
-var = 0;
-for t = 1:6
-    var = var + s_kt_max(1,t)*n_t(t);
-end
-H(14,59) = var;
-var = 0;
-for t = 1:6
-    var = var + s_kt_max(2,t)*n_t(t);
-end
-H(15,60) = var;
-var = 0;
-for t = 1:6
-    var = var + s_kt_max(3,t)*n_t(t);
-end
-H(16,61) = var;
-
-f = c_prime'*H;
-[X, FVAL, EXITFLAG] = linprog(-f,A,b);
-
-%{
 
 classdef power_system < handle
     
@@ -175,13 +18,95 @@ classdef power_system < handle
     %}
     
     properties (Access = private)
+        
+        x_i_max
+        x_i_min
+        o_i_u
+        o_i_p
+        g_i
+        c_i_v
+        sigma_i_v
+        c_i_c
+        sigma_i_c
+        n_t
+        l
+        s_kt_max
+        c_k_d
+        sigma_k_d
+        
+        A
+        b
+        
+        x_ti
+        y_i
+        z_k
+        
+        cost_c_o
+        cost_H
+        cost_min_f
+        cost_max_f
     end
     
     methods(Access = public)
         
-        
+        function obj = power_system(x_i_max, x_i_min, o_i_u, o_i_p, ...
+                g_i, c_i_v, sigma_i_v, c_i_c, sigma_i_c, n_t, l, ... 
+                s_kt_max, c_k_d, sigma_k_d)
+            
+            %{
+                class constructor
+                :param x_i_max: vector of plant maximum capacity
+                :param x_i_min: vector of plant minimum capacity
+                :param o_i_u: vector of unplanned outage rate
+                :param o_i_p: vector of planned annual outage rate
+                :param g_i: vector of global warming potential
+                :param c_i_v: vector variable cost of plant operation
+                :param sigma_i_v: vector of standard deviation of variable
+                                  cost of plant operation
+                :param c_i_c: vector of annaulized capital cost of plant
+                :param sigma_i_c: vector of standard deviation of 
+                                  annualized capital cost of plant
+                :param n_t: array of number of hours in load block
+                :param l: array of power load in load block
+                :param s_kt_max: matrix of maximum energy saving for each
+                                 load block and demand side managemenr
+                                 program
+                :param c_k_d: vector of cost of implementation of demand
+                              side management program
+                :param sigma_k_d: vector of cost standard deviation of 
+                                  implementation of demand side management
+                                  program
+                :return:
+            %}
+
+            % Set given parameters
+            obj.x_i_max = x_i_max;
+            obj.x_i_min = x_i_min;
+            obj.o_i_u = o_i_u;
+            obj.o_i_p = o_i_p;
+            obj.g_i = g_i;
+            obj.c_i_v = c_i_v;
+            obj.sigma_i_v = sigma_i_v;
+            obj.c_i_c = c_i_c;
+            obj.sigma_i_c = sigma_i_c;
+            obj.n_t = n_t;
+            obj.l = l;
+            obj.s_kt_max = s_kt_max;
+            obj.c_k_d = c_k_d;
+            obj.sigma_k_d = sigma_k_d;
+            
+            obj.construct_A_b()
+            obj.gen_cost_obj_funs()
+            %obj.gen_emi_obj_funs()
+        end
        
         
+        
+        %{
+        function cost = minimize_cost()
+         
+        end
+         %}  
         % optimize for expected cost
         % get cost given f
         % get emissions given f
@@ -206,9 +131,181 @@ classdef power_system < handle
         
     end
         
-    methods(Access = private)    
+    methods(Access = private)
+                
+        function construct_A_b(obj)
+            %{
+                This function constructs matrix A for solving the
+                inequality constraints Ax<=b (A is 157x61, b is 1x157)
+                :param:
+                :return:
+            %}
+            
+            % space to store A and b 
+            obj.A = zeros(157,61);
+            obj.b = NaN(157,1);
+            
+            % matrix to store x_it indices for each row in A 
+            obj.x_ti = zeros(6,9);
+            for i = 1:numel(obj.x_ti)
+                obj.x_ti(i) = i; 
+            end    
+            % vector to store y_i indices for each row in A
+            obj.y_i = NaN(1,9)';
+            obj.y_i(5:8) = [55 56 57 58]';
+            % vector to store z_k indices for each row in A
+            obj.z_k = [59 60 61];
+            
+            % building matrix A and vector b
+            idx = 1;
+            % CONSTRAINT SET 1
+            % load constraint (6 rows)
+            % sum_i=1^9{x_it} + sum_k=1^3{s_kt_max} >= l_t for t={1,2,3,4,5,6}
+            for t = 1:6
+                obj.A(idx,obj.x_ti(t,:)) = -1;
+                obj.A(idx,obj.z_k) = -obj.s_kt_max(:,t)';
+                obj.b(idx) = -obj.l(t);
+                idx = idx + 1;
+            end
+            
+            % CONSTRAINT SET 2
+            % instantaneous capacity constraint (54 rows)
+            % x_it <= (1-o_i_u)x_i_max for i={1,2,3,4,5,6,7,8,9}, t={1,2,3,4,5,6}
+            for i = 1:9
+                for t = 1:6
+                    obj.A(idx,obj.x_ti(t,i)) = 1;
+                    obj.b(idx) = (1-obj.o_i_u(i))*obj.x_i_max(i);
+                    idx = idx + 1; 
+                end
+            end 
+            
+            % CONSTRAINT SET 3
+            % annual energy constraint for exisiting plants (5 rows)
+            % sum_t=1^6{n_tx_it} - (1-o_i_p)8766x_i_max <= for i={1,2,3,4,9}
+            for i = 1:4
+                obj.A(idx,obj.x_ti(:,i)') = obj.n_t;
+                obj.b(idx) = (1-obj.o_i_p(i))*obj.x_i_max(i)*8766;
+                idx = idx + 1;
+            end
+            obj.A(idx,obj.x_ti(:,9)') = obj.n_t;
+            obj.b(idx) = (1-obj.o_i_p(9))*obj.x_i_max(9)*8766;
+            idx = idx + 1;
+            
+            % CONSTRAINT SET 4
+            % annual energy constraint for new plants (4 rows)
+            % sum_t=1^6{n_tx_it} - (1-o_i_p)8766y_i <= for i={5,6,7,8}
+            for i = 5:8
+                obj.A(idx,obj.x_ti(:,i)') = obj.n_t;
+                obj.A(idx,obj.y_i(i)) = -8766*(1-obj.o_i_p(i));
+                obj.b(idx) = 0;
+                idx = idx + 1;
+            end
+            
+            % CONSTRAINT SET 5
+            % ainimum generation constraint (54 rows)
+            % x_it >= x_i_min for i={1,2,3,4,5,6,7,8,9}, t={1,2,3,4,5,6}
+            for i = 1:9
+                for t = 1:6
+                    obj.A(idx,obj.x_ti(t,i)) = -1;
+                    obj.b(idx) = -obj.x_i_min(i);
+                    idx = idx + 1;
+                end
+            end 
+            
+            % CONSTRAINT SET 6
+            % lower bound on DSM program (3 rows)
+            % z_k >= 0 for k={1,2,3}
+            for k = 1:3
+                obj.A(idx,obj.z_k(k)) = -1;
+                obj.b(idx) = 0;
+                idx = idx + 1;
+            end
+            
+            % CONSTRAINT SET 7
+            % upper bound on DSM program (3 rows)
+            % z_k <= 1 for k={1,2,3}
+            for k = 1:3
+                obj.A(idx,obj.z_k(k)) = 1;
+                obj.b(idx) = 1;
+                idx = idx + 1;
+            end
+            
+            % CONSTRAINT SET 8
+            % new generation bounds (24 rows)
+            % x_it <= (1-o_i_u)y_i for i={5,6,7,8}, t={1,2,3,4,5,6}
+            for i = 5:8
+                for t = 1:6
+                    obj.A(idx,obj.x_ti(t,i)) = 1;
+                    obj.A(idx,obj.y_i(i)) = -(1-obj.o_i_u(i));
+                    obj.b(idx) = 0;
+                    idx = idx + 1;
+                end
+            end 
+            
+            % CONSTRAINT SET 9
+            % new generation bounds (4 rows)
+            % y_i <= x_i_max for i={5,6,7,8}
+            for i = 5:8
+                obj.A(idx,obj.y_i(i)) = 1;
+                obj.b(idx) = obj.x_i_max(i);
+                idx = idx + 1;
+            end
+            
+        end
+        
+        function gen_cost_obj_funs(obj)
+            %{
+                This function generates the cost objective functions (for
+                both minimization and maximization), along with the vectors
+                and matrices needed to obtain the variance
+                :param:
+                :return:
+            %}
+            
+            % set up cost objective function
+            obj.cost_c_o = [obj.c_i_v; obj.c_i_c(5:8); obj.c_k_d];
+            obj.cost_H = zeros(numel(obj.cost_c_o),size(obj.A,2));
+            v_idx = 1;
+            h_idx = 1;
+
+            % variable cost component
+            % sum_i=1^9{sum_t=1^6{c_i_v n_t x_it}}
+            for i = 1:9
+                obj.cost_H(v_idx,1+numel(obj.n_t)*(i-1):numel(obj.n_t)+ ... 
+                    numel(obj.n_t)*(i-1)) = obj.n_t;
+                v_idx = v_idx + 1;
+                h_idx = h_idx + numel(obj.n_t);
+            end
+
+            % capital cost component
+            % sum_i=5^8{c_i_vy_i10^3}
+            for i = 5:8
+                obj.cost_H(v_idx,h_idx) = 1000;
+                v_idx = v_idx + 1;
+                h_idx = h_idx + 1;
+            end
+
+            % demand side management cost component
+            % sum_k=1^3{sum_t=1^6{c_k_d z_k s_kt}}
+            for k = 1:3
+                val = 0;
+                for t = 1:6
+                    val = val + obj.s_kt_max(k,t)*obj.n_t(t);
+                end
+                obj.cost_H(v_idx,h_idx) = val;
+                v_idx = v_idx + 1;
+                h_idx = h_idx + 1;
+            end
+            
+            % set cost min/max vector for linprog
+            obj.cost_min_f = obj.cost_c_o'*obj.cost_H;
+            obj.cost_max_f = -obj.cost_c_o'*obj.cost_H;
+            
+        end
+        
+        function gen_emi_obj_funs(obj)
+            a = 2;
+        end
     end
     
 end
-
-%}
